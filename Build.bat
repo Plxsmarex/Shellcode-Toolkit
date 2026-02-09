dd5ae890c710c1f9bbdb98a074537b617de01307
Plxsmarex <securedbyplasmarex@gmail.com>
2026-02-09 14:22:12 +0000

    Build with the linker script

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 @color 2
-@gcc -o Program.exe Main.c -e EntryPoint -Os -s -fPIC -Wl,--gc-sections -ffunction-sections -fno-asynchronous-unwind-tables -fno-ident -ffreestanding -nostartfiles -nostdlib -mno-stack-arg-probe
+@gcc -o Program.exe Main.c -e EntryPoint -T Linker.ld -Os -s -fPIC -Wl,--gc-sections -ffunction-sections -fno-asynchronous-unwind-tables -fno-ident -ffreestanding -nostartfiles -nostdlib -mno-stack-arg-probe
 @objcopy -j .text -O binary Program.exe Program.bin
 @pause
