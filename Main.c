34bb5ddca0c6d97f4b47850c32e2f462c5d70307
Plxsmarex <securedbyplasmarex@gmail.com>
2026-02-09 14:09:45 +0000

    Small changes to style of code

--- Main.c
+++ Main.c
@@ -1,7 +1,7 @@
 #include "Shellcode-Toolkit.c"
 
 // createstring will put the string in the .text section
-#define createstring __attribute__((section(".text."), used))
+#define createstring __attribute__((section(".text.data"), used))
 
 // Strings
 createstring char User32[] = "USER32";
@@ -14,15 +14,15 @@ int EntryPoint() {
 	unsigned int User32Hash = 0x2208CF13; // "USER32.DLL"
 	unsigned int LoadLibraryAHash = 0x5FBFF0FB; // "LoadLibraryA"
 	unsigned int MessageBoxAHash = 0x384F14B4; // "MessageBoxA"
-	// unsigned int ExitProcessHash = 0xB769339E; // "ExitProcess"
+	//unsigned int ExitProcessHash = 0xB769339E; // "ExitProcess"
 
 	// Get PEB and Kernel32 base
 	void *PEBBase = GetPEBBase();
 	void *Kernel32BaseAddress = GetDLLBase(PEBBase, Kernel32Hash);
 
 	// Get LoadLibraryA function
-	void *LoadLibraryAPointer  = GetExportAddress(Kernel32BaseAddress, LoadLibraryAHash);
-	typedef void *(__stdcall *LoadLibraryAType)(const char *);
+	void *LoadLibraryAPointer = GetExportAddress(Kernel32BaseAddress, LoadLibraryAHash);
+	typedef void (*LoadLibraryAType)(const char *lpLibFileName);
 	LoadLibraryAType LoadLibraryA = (LoadLibraryAType)LoadLibraryAPointer;
 
 	// Load USER32.DLL (Loading modules from shellcode like this is very suspicious to EDR, only included for this example)
@@ -31,17 +31,17 @@ int EntryPoint() {
 
 	// Get MessageBoxA function
 	void *MessageBoxAPointer = GetExportAddress(User32BaseAddress, MessageBoxAHash);
-	typedef int (__stdcall *MessageBoxAType)(void* hWnd, char* lpText, char* lpCaption, unsigned int uType);
+	typedef int (*MessageBoxAType)(void *hWnd, char *lpText, char *lpCaption, unsigned int uType);
 	MessageBoxAType MessageBoxA = (MessageBoxAType)MessageBoxAPointer;
 
 	// Get ExitProcess function
-	// void *ExitProcessPointer = GetExportAddress(Kernel32BaseAddress, ExitProcessHash);
-	// typedef void (__stdcall *ExitProcessType)(unsigned int uExitCode);
-	// ExitProcessType ExitProcess = (ExitProcessType)ExitProcessPointer;
+	//void *ExitProcessPointer = GetExportAddress(Kernel32BaseAddress, ExitProcessHash);
+	//typedef void (*ExitProcessType)(unsigned int uExitCode);
+	//ExitProcessType ExitProcess = (ExitProcessType)ExitProcessPointer;
 
 	// Call the messagebox
 	MessageBoxA(0, Message, Title, 0);
 
 	// Exit the process
-	// ExitProcess(0);
+	//ExitProcess(0);
 }
