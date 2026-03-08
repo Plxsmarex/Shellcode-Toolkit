6d37646e54ad7edff6ebde4d010d07c824d46f2b
Plxsmarex <201103443+Plxsmarex@users.noreply.github.com>
2026-03-08 15:36:30 +0000

    Fix crash, compress code, -64 bytes compilesize

--- Main.c
+++ Main.c
@@ -10,39 +10,21 @@ createstring char Message[] = "Hello, world!";
 
 __attribute__((section(".text.entrypoint"), used))
 int EntryPoint() {
-	// Function and module hashes
-	unsigned int Kernel32Hash = 0x6DDB9555; // "KERNEL32.DLL"
-	unsigned int User32Hash = 0x2208CF13; // "USER32.DLL"
-	unsigned int LoadLibraryAHash = 0x5FBFF0FB; // "LoadLibraryA"
-	unsigned int MessageBoxAHash = 0x384F14B4; // "MessageBoxA"
-	//unsigned int ExitProcessHash = 0xB769339E; // "ExitProcess"
-
 	// Get PEB and Kernel32 base
 	void *PEBBase = GetPEBBase();
-	void *Kernel32BaseAddress = GetDLLBase(PEBBase, Kernel32Hash);
+	void *Kernel32BaseAddress = GetDLLBase(PEBBase, 0x6DDB9555); // "KERNEL32.DLL"
 
 	// Get LoadLibraryA function
-	void *LoadLibraryAPointer = GetExportAddress(Kernel32BaseAddress, LoadLibraryAHash);
-	typedef void (*LoadLibraryAType)(const char *lpLibFileName);
-	LoadLibraryAType LoadLibraryA = (LoadLibraryAType)LoadLibraryAPointer;
+	typedef void *(*LoadLibraryAType)(const char *lpLibFileName);
+	LoadLibraryAType LoadLibraryA = (LoadLibraryAType)GetExportAddress(Kernel32BaseAddress, 0x5FBFF0FB); // "LoadLibraryA"
 
-	// Load USER32.DLL (Loading modules from shellcode like this is very suspicious to EDR, only included for this example)
-	LoadLibraryA(User32);
-	void *User32BaseAddress = GetDLLBase(PEBBase, User32Hash);
+	// Load USER32.DLL
+	void *User32BaseAddress = LoadLibraryA(User32);
 
 	// Get MessageBoxA function
-	void *MessageBoxAPointer = GetExportAddress(User32BaseAddress, MessageBoxAHash);
 	typedef int (*MessageBoxAType)(void *hWnd, char *lpText, char *lpCaption, unsigned int uType);
-	MessageBoxAType MessageBoxA = (MessageBoxAType)MessageBoxAPointer;
-
-	// Get ExitProcess function
-	//void *ExitProcessPointer = GetExportAddress(Kernel32BaseAddress, ExitProcessHash);
-	//typedef void (*ExitProcessType)(unsigned int uExitCode);
-	//ExitProcessType ExitProcess = (ExitProcessType)ExitProcessPointer;
+	MessageBoxAType MessageBoxA = (MessageBoxAType)GetExportAddress(User32BaseAddress, 0x384F14B4); // "MessageBoxA"
 
 	// Call the messagebox
 	MessageBoxA(0, Message, Title, 0);
-
-	// Exit the process
-	//ExitProcess(0);
 }
