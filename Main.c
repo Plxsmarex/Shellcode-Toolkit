d56ef6afc25d86908b8c4134062a5e12e9d5449f
Plxsmarex <201103443+Plxsmarex@users.noreply.github.com>
2026-03-18 18:56:20 +0000

    Who let him cook

--- Main.c
+++ Main.c
@@ -1,30 +1,32 @@
-#include "Shellcode-Toolkit.c"
+#include "Shellcode-Toolkit.h"
 
-// createstring will put the string in the .text section
-#define createstring __attribute__((section(".text.data"), used))
+// Function types
+typedef void *(*Type_LoadLibraryA)(const char *lpLibFileName);
+typedef int   (*Type_MessageBoxA)(void *hWnd, char *lpText, char *lpCaption, unsigned int uType);
 
 // Strings
-createstring char User32[] = "USER32";
-createstring char Title[] = "Shellcode Test";
+createstring char User32[]  = "USER32";
+createstring char Title[]   = "Shellcode Test";
 createstring char Message[] = "Hello, world!";
 
-__attribute__((section(".text.entrypoint"), used))
-int EntryPoint() {
-	// Get PEB and Kernel32 base
-	void *PEBBase = GetPEBBase();
-	void *Kernel32BaseAddress = GetDLLBase(PEBBase, 0x6DDB9555); // "KERNEL32.DLL"
+// Entry point
+int main(void)
+{
+	// Get address of LoadLibraryA in KERNEL32.DLL, we will use this to load USER32.DLL, which contains the MessageBoxA function
+	void *Address_PEB = GetPEBAddress();
+	void *Address_KERNEL32 = GetModuleAddress(Address_PEB, 0x76918253); // "KERNEL32.DLL"
+	void *Address_LoadLibraryA = GetExportAddress(Address_KERNEL32, 0x139A2F01); // "LoadLibraryA"
 
-	// Get LoadLibraryA function
-	typedef void *(*LoadLibraryAType)(const char *lpLibFileName);
-	LoadLibraryAType LoadLibraryA = (LoadLibraryAType)GetExportAddress(Kernel32BaseAddress, 0x5FBFF0FB); // "LoadLibraryA"
+	// Get ready to call LoadLibraryA
+	Type_LoadLibraryA LoadLibraryA = (Type_LoadLibraryA)Address_LoadLibraryA;
 
-	// Load USER32.DLL
-	void *User32BaseAddress = LoadLibraryA(User32);
+	// Call it and load USER32.DLL, LoadLibraryA should return its address
+	void *Address_USER32 = LoadLibraryA(User32);
 
-	// Get MessageBoxA function
-	typedef int (*MessageBoxAType)(void *hWnd, char *lpText, char *lpCaption, unsigned int uType);
-	MessageBoxAType MessageBoxA = (MessageBoxAType)GetExportAddress(User32BaseAddress, 0x384F14B4); // "MessageBoxA"
+	// Get ready to call MessageBoxA
+	Type_MessageBoxA MessageBoxA = (Type_MessageBoxA)GetExportAddress(Address_USER32, 0xDDF06316); // "MessageBoxA"
 
 	// Call the messagebox
 	MessageBoxA(0, Message, Title, 0);
+	return 0;
 }
