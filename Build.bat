d6a9664d8136322a21962e18b541d14a2d37f282
Plxsmarex <201103443+Plxsmarex@users.noreply.github.com>
2026-03-18 18:58:59 +0000

    Remove bogus compile arguments

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
-@gcc -o Program.exe Main.c -e EntryPoint -T Linker.ld -Os -s -fPIC -Wl,--gc-sections -ffunction-sections -fno-asynchronous-unwind-tables -fno-ident -ffreestanding -nostartfiles -nostdlib -mno-stack-arg-probe
+@gcc -o Program.exe Main.c -T Linker.ld -e EntryPoint -Os -s -ffunction-sections -fno-asynchronous-unwind-tables -fno-ident -ffreestanding -nostartfiles -nostdlib -mno-stack-arg-probe
 @objcopy -j .text -O binary Program.exe Program.bin
 @pause
