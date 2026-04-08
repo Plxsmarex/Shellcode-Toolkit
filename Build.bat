f20aa0e53e4c0d43a46cb514a3caaba5a3caf8dd
Plxsmarex <201103443+Plxsmarex@users.noreply.github.com>
2026-04-08 20:18:53 +0100

    Link to libc for embedded functions and the stack prober

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
-@gcc -o Program.exe Main.c -e EntryPoint -T Linker.ld -Oz -s -ffunction-sections -Wl,--gc-sections -fno-asynchronous-unwind-tables -nostartfiles -nostdlib -mno-stack-arg-probe
+@gcc -o Program.exe Main.c -e EntryPoint -T Linker.ld -Oz -s -ffunction-sections -Wl,--gc-sections -fno-asynchronous-unwind-tables -nostartfiles
 @objcopy -j .text -O binary Program.exe Program.bin
 @pause
